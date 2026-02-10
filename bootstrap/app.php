72306680680b91401a75c3a1dc27673209160744
Mustafa <mustafadirlikli@gmail.com>
2026-02-10 20:54:34 +0300

    admin paneli url si düzeldi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
+        // Giriş yapılmamışsa admin sayfalarında giriş sayfasına yönlendir
+        $middleware->redirectGuestsTo(fn (Request $request) => route('admin.login'));
+        // Giriş yapmış kullanıcı /admin/giris'e gelirse panele yönlendir
+        $middleware->redirectUsersTo(function (Request $request) {
+            if ($request->is('admin/giris')) {
+                return route('admin.dashboard');
+            }
+            return '/';
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
